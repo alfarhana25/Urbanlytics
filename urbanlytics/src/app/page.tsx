2f5bea8229e41b4e14f4369c7a2d290aae767ac3
Aly Masani <alymohammed.masani@ucalgary.ca>
2025-11-08 22:38:53 -0700

    fixed top bar with map and filters

--- urbanlytics/src/app/page.tsx
+++ urbanlytics/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
   return (
     <main className="h-dvh w-dvw overflow-hidden bg-zinc-950 text-neutral-100">
       <TopBar />
-      <div className="pt-14 h-[calc(100vh-56px)] flex">
+      <div className="pt-14 + calc(100vh-56px) flex">
         {/* Map area fills all remaining height below TopBar */}
         <div className="flex-1 relative overflow-hidden">
           <RiskMap />
